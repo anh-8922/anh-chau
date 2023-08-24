5f03b80e6641c07da8b0950575f2d2bb4d041e5c
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-08-24 13:37:34 +0100

    updated

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {styledemo} from './styles/styles.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world</Text>
+      <View style={styledemo.box}>
+          <Text>testing</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ styles/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styledemo = StyleSheet.create({
+
+box: {
+    width: '80%',
+    height: 150,
+    backgroundColor: 'green',
+    alignSelf: 'center',
+    borderRadius: 9
+  }
+   
+});
+
+export {styledemo}
